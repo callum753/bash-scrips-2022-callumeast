a006d71e04090485f20645b8960e088855b1c168
callum753 <c0013851@my.shu.ac.uk>
2022-12-22 11:23:29 +0000

    bash script

--- bash.sh
+++ bash.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
-echo "bash version" ${BASH_VERSION}...
-for i in {0..10..2}
-do 
-echo "welcome $i times"
+for i in {3..1}
+do
+   echo "installing LAMP techonagle in: $1"
+   sleep 1
 done 
+
+
+#LAMP techonagle 
+sudo apt update 
+sudo apt upgrade -y
+sudo apt install apache2 -y
+sudo systemctl status apache2
+sudo apt install mariadb-server mariadb-client
+sudo systemctl status mariadb
+sudo mysql_secure_installation 
+sudo apt install php-y
+a2enmod php8.1 
+
+#tree
+apt install tree 
+cd 
+cd/home 
+tree 
+
+
+
